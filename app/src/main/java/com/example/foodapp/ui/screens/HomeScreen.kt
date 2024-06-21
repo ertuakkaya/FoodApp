b21581d4d03586753f26b95cdd529bf14180dde6
ertuakkaya <ertuakkaya@gmail.com>
2024-06-21 18:14:29 +0300

    TODO added

--- app/src/main/java/com/example/foodapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/foodapp/ui/screens/HomeScreen.kt
@@ -1,31 +1,23 @@
 package com.example.foodapp.ui.screens
 
 import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -35,12 +27,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,7 +36,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
-import coil.compose.rememberImagePainter
 import com.example.foodapp.R
 import com.example.foodapp.data.ResourceState
 import com.example.foodapp.data.entitiy.Yemekler
@@ -104,6 +91,7 @@ fun YemekKart(yemek: Yemekler) {
                     .height(150.dp)
                     .clickable {
                         // TODO: YemekDetayScreen'e git
+                        //TODO: (yemekDetayScreen(yemek.yemek_adi,yemek.yemek_fiyat,yemek.yemek_resim_adi,yemek.yemek_id))
                     }
                     .fillMaxWidth(),
 
@@ -150,8 +138,9 @@ fun YemekKart(yemek: Yemekler) {
                 // Sepete Ekle Butonu
                 IconButton(
                     onClick = {
-                              // TODO: Sepete Ekle
+                              // TODO: Sepete Ekle api çağrısı yapılacak
                               // TODO: Sepete ekleme animasyonunu getir
+
                     },
                     modifier = Modifier
                         .wrapContentSize()
@@ -205,7 +194,11 @@ fun YemekListesi(yemekler: List<Yemekler>) {
 }
 
 
+@Composable
+fun yemekDetayScreen(yemekAdi: String, yemekFiyat: Int, yemekResimAdi: String, yemekId: Int) {
+    // TODO: Yemek Detay Ekranı parametre olarak
 
+}
 
 
 @Composable
