0ec854b2b0b20f8582794ca73bc7ea39c94e971f
ertuakkaya <ertuakkaya@gmail.com>
2024-06-23 12:10:09 +0300

    corotineScope is added

--- app/src/main/java/com/example/foodapp/ui/screens/SepetScreen.kt
+++ app/src/main/java/com/example/foodapp/ui/screens/SepetScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -47,184 +48,8 @@ import com.example.foodapp.data.ResourceState
 import com.example.foodapp.data.entitiy.SepetYemekler
 import com.example.foodapp.data.entitiy.Yemekler
 import com.example.foodapp.ui.viewmodel.YemeklerViewModel
-
-
-/*
-@Composable
-fun SepetScreen(yemeklerViewModel: YemeklerViewModel = hiltViewModel()) {
-    val sepetYemeklerResponse by yemeklerViewModel.sepetYemekler.collectAsState()
-
-
-    when(sepetYemeklerResponse){
-        is ResourceState.Loading -> {
-
-            val loading = (sepetYemeklerResponse as ResourceState.Loading)
-            Log.d("SepetScreen", "SepetScreen: Loading... $loading")
-        }
-        is ResourceState.Success -> {
-            val response = (sepetYemeklerResponse as ResourceState.Success).data
-            Log.d("SepetScreen", "SepetScreen: SUCCESS... success = ${response.success} | yemekeler.size = ${response.sepet_yemekler.size}")
-
-            if(response.sepet_yemekler.isNotEmpty()){
-
-                //
-                SepetListesi(yemekler = response.sepet_yemekler)
-
-            }else{
-                // TODO("EmptyStateComponent() buraya yemeklerin yuklenmedigi senaryo eklenecek")
-            }
-        }
-        is ResourceState.Error -> {
-            val error = (sepetYemeklerResponse as ResourceState.Error)
-            Log.d("SepetScreen", "SepetScreen: Error... $error")
-        }
-    }
-}
-
-*/
-/*
-@Composable
-fun SepetKart(yemek: SepetYemekler,yemeklerViewModel: YemeklerViewModel = hiltViewModel()) { // TODO: viewmodel parametresi eklenecek
-
-    val response by yemeklerViewModel.sepetYemekler.collectAsState()
-
-    when(response){
-
-        is ResourceState.Loading -> {
-
-            Log.d("Sepete Ekeleme", "Sepete Ekeleme: Loading...")
-
-        }
-        is ResourceState.Success -> {
-            val response = (response as ResourceState.Success).data
-            Log.d("Sepete Ekeleme", "Sepete Ekeleme: SUCCESS... success = ${response.success} | message = ${response.sepet_yemekler.size}")
-
-        }
-        is ResourceState.Error -> {
-            val error = (response as ResourceState.Error)
-            Log.d("Sepete Ekeleme", "Sepete Ekeleme: Error... $error")
-        }
-    }
-
-    Card(
-        shape = RoundedCornerShape(8.dp),
-        modifier = Modifier
-            .padding(8.dp)
-            .wrapContentSize(),
-        elevation =  CardDefaults.cardElevation(4.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(8.dp)
-        ) {
-            // Yemek Resmi
-            AsyncImage(
-                model = "http://kasimadalan.pe.hu/yemekler/resimler/${yemek.yemek_resim_adi}",
-                contentDescription = yemek.yemek_adi,
-                modifier = Modifier
-                    .height(150.dp)
-                    .clickable {
-                        // TODO: YemekDetayScreen'e git
-                        //TODO: (yemekDetayScreen(yemek.yemek_adi,yemek.yemek_fiyat,yemek.yemek_resim_adi,yemek.yemek_id))
-
-
-                    }
-                    .fillMaxWidth(),
-
-                contentScale = ContentScale.Fit
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-
-            // Yemek Adı
-            Text(
-                text = yemek.yemek_adi,
-                modifier = Modifier
-                    //.fillMaxWidth()
-                    .align(Alignment.CenterHorizontally)
-                    .size(width = 150.dp, height = 30.dp),
-
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.SemiBold
-
-
-
-            )
-            // Yemek Fiyatı ve Sepete Ekle Butonu
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(),
-
-                horizontalArrangement = Arrangement.SpaceBetween
-
-            ){
-                // Yemek Fiyatı
-                Text(
-                    text = "${yemek.yemek_fiyat} TL",
-                    color = Color.Red,
-                    modifier = Modifier
-                        .weight(1f)
-                        .align(Alignment.CenterVertically)
-                        //.border(1.dp, Color.Red, RectangleShape)
-                        .padding(start = 8.dp),
-                    fontSize = 20.sp
-
-                )
-
-                // Sepete Ekle Butonu
-                IconButton(
-                    onClick = {
-                        // TODO: Sepete Ekle api çağrısı yapılacak
-                        // TODO: Sepete ekleme animasyonunu getir
-                        // TODO: viewmodelden sepete ekle fonksiyonu çağrılacak ////
-
-                        yemeklerViewModel.sepeteYemekEkle(yemek.yemek_adi, yemek.yemek_resim_adi, yemek.yemek_fiyat, 1)
-
-
-
-                    },
-                    modifier = Modifier
-                        .wrapContentSize()
-                    //.size(25.dp),
-
-
-                )
-                {
-                    Icon(
-                        painter = painterResource(id = R.drawable.basket),
-                        contentDescription = "",
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
-
-
-            }
-        }
-    }
-}
-*/
-
-
-
-
-@Composable
-fun SepetListesi(yemekler: List<SepetYemekler>) {
-
-
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(1),
-        modifier = Modifier
-            //.fillMaxSize()
-            .fillMaxWidth()
-            //.padding(top = 100.dp, bottom = 100.dp)
-            .background(Color.LightGray)
-
-    ) {
-
-        items(yemekler.size) { yemek ->
-            SepetKart(yemek = yemekler[yemek])
-        }
-    }
-}
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -259,9 +84,39 @@ fun SepetScreen(yemeklerViewModel: YemeklerViewModel = hiltViewModel()) {
 
 
 
+@Composable
+fun SepetListesi(yemekler: List<SepetYemekler>) {
+
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(1),
+        modifier = Modifier
+            //.fillMaxSize()
+            .fillMaxWidth()
+            //.padding(top = 100.dp, bottom = 100.dp)
+            .background(Color.LightGray)
+
+    ) {
+
+        items(yemekler.size) { yemek ->
+            SepetKart(yemek = yemekler[yemek])
+        }
+    }
+}
+
+
+
+
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SepetKart(yemek: SepetYemekler, yemeklerViewModel: YemeklerViewModel = hiltViewModel()) {
+
+    // coroutine scope
+    val coroutineScope = rememberCoroutineScope()
+
+
     val response by yemeklerViewModel.sepetYemekler.collectAsState()
 
     when (response) {
@@ -390,9 +245,13 @@ fun SepetKart(yemek: SepetYemekler, yemeklerViewModel: YemeklerViewModel = hiltV
                 // Sepet Sil Butonu
                 IconButton(
                     onClick = {
-                        yemeklerViewModel.sepettenYemekSil(yemek.sepet_yemek_id)
-                        // sepetteki yemkleri tekrar cek
-                        yemeklerViewModel.sepettekiYemekleriGetir()
+                        coroutineScope.launch {
+                            yemeklerViewModel.sepettenYemekSil(yemek.sepet_yemek_id)
+                            // sepetteki yemkleri tekrar cek
+                            delay(500)
+                            yemeklerViewModel.sepettekiYemekleriGetir()
+                        }
+
                     },
                     modifier = Modifier
                         .size(50.dp)
